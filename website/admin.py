6550131005192ddfd008b4ac69dac99fe4f8f52b
Prateek Thakur <prateekthakur272@gmail.com>
2024-04-17 13:28:27 +0530

    adds record model

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Record
 
-# Register your models here.
+# admin.site.register(Record)
+@admin.register(Record)
+class AdminRecord(ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email', 'city']
